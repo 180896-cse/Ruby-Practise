9cd3a00a9fc07717621f3cf4e4251bf3749ed165
shantanupratap <shantanu.pratap@nagarro.com>
2023-07-09 19:00:03 +0530

    Common Module Created

---
+++ Practise-Set1/cmmn_modules.rb
@@ -0,0 +1,16 @@
+
+module Disp
+    def self.printUsrInfo(*rest)
+    puts rest
+    end
+end
+ 
+module Mfunc
+    def self.getUsrInput()
+        return @usrData = gets;
+    end;  
+end;
+
+module Mcntrl
+
+end
